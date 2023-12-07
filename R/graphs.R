269eccb83e16155a77a0539be425cf110da0cca2
Anatoly Sorokin <lptolik@gmail.com>
2023-12-07 11:16:24 +0900

    add type of the network

--- R/graphs.R
+++ R/graphs.R
@@ -26,17 +26,24 @@ graphFromSynaptomeByEntrez<-function(entrez,LCC=TRUE,simplify=TRUE){
 #'        \code{\link{getGenesByID}}
 #' @param LCC if TRUE only largest connected component is returned
 #' @param simplify if TRUE loops and multiple edges will be removed
+#' @param type type of the PPI network should be either `induced` (for
+#'     all the PPIs for specific genes, including external genes) or
+#'     `limited` (for PPIs between the genes specified in the query).
+#'     Type could be shortened to recognizable minimum like 'ind'
+#'     or 'lim'.
 #'
 #' @return network defined by the gene table
 #' @export
+#' @seealso [getPPIbyIDs()]
 #'
 #' @examples
 #' library(synaptome.db)
 #' cid<-match('Presynaptic', getCompartments()$Name)
 #' geneTable<-getAllGenes4Compartment(cid)
 #' gg<-graphFromSynaptomeGeneTable(geneTable)
-graphFromSynaptomeGeneTable<-function(geneTable,LCC=FALSE,simplify=FALSE){
-    p<-getPPIbyIDs(geneTable$GeneID, type = 'limited')
+graphFromSynaptomeGeneTable<-function(geneTable,LCC=FALSE,simplify=FALSE,
+                                      type = c("induced", "limited")){
+    p<-getPPIbyIDs(geneTable$GeneID, type = type)
     aidx<-match(p$A, geneTable$GeneID)
     bidx<-match(p$B, geneTable$GeneID)
     # TODO: uncomment once BioNAR>= 1.3.7 in Bioconductor
